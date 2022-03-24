12b40c6ff2ec4fd45bb9add7bb6554f28cdd37ef
marcelodonato <62271815+MarceloDonato@users.noreply.github.com>
2022-03-24 09:50:41 -0300

    Initial commit

---
+++ app/src/main/java/com/marcelodonato/instagram/main/view/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.marcelodonato.instagram.main.view
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsetsController
+import androidx.annotation.RequiresApi
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import com.marcelodonato.instagram.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.setSystemBarsAppearance(
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+            )
+        }
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.gray)
+
+        val toolbar = findViewById<Toolbar>(R.id.main_toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_insta_camera)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = ""
+
+
+    }
+}
---
+++ app/src/main/java/com/marcelodonato/instagram/register/view/RegisterEmailFragment.kt
@@ -0,0 +1,15 @@
+package com.marcelodonato.instagram.register.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.marcelodonato.instagram.R
+
+class RegisterEmailFragment : Fragment()  {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_register_email, container, false )
+    }
+}
